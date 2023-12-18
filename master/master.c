14f8cd0603e4de1142e235fb896c85fa4b01adec
ZiHan Huang <zack.huang@rock-chips.com>
2023-12-18 09:36:54 +0800

    Revert "solve complie err"
    This reverts commit 97ab2a09ce9c2048310d6b358cc6c8e181c981cb.

--- master/master.c
+++ master/master.c
@@ -2100,7 +2100,7 @@ const ec_slave_config_t *ec_master_get_config_const(
  * \return Number of domains.
  */
 unsigned int ec_master_domain_count(
-			 ec_master_t *master /**< EtherCAT master. */
+        const ec_master_t *master /**< EtherCAT master. */
         )
 {
     const ec_domain_t *domain;
