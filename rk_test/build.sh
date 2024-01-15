e9b543f58b53aa9abb50dc323c0c979d4225d802
ZiHan Huang <zack.huang@rock-chips.com>
2024-01-15 20:44:45 +0800

    rk_test: Distinguish between two-motor examples and single-motor examples
    Change-Id: I233622399b952e9bacc6af76905a15f39ef32393
    Signed-off-by: ZiHan Huang <zack.huang@rock-chips.com>

--- rk_test/build.sh
+++ rk_test/build.sh
@@ -2,4 +2,6 @@ export PATH=~/linux-develop/3568/buildroot/output/rockchip_rk3568/host/bin:$PATH
 
 aarch64-buildroot-linux-gnu-gcc Rockchip_MADHT1505BA1.c -I ../include/ -L ../lib/.libs -g -lethercat -shared -fPIC -o libmadht1505ba1.so
 
+aarch64-buildroot-linux-gnu-gcc rk_test_two.c  -I ../include/ -L ../lib/.libs -L ./ -g -lmadht1505ba1 -lethercat -o rk_test_two
+
 aarch64-buildroot-linux-gnu-gcc rk_test.c  -I ../include/ -L ../lib/.libs -L ./ -g -lmadht1505ba1 -lethercat -o rk_test
--- rk_test/rk_test_two.c
+++ rk_test/rk_test_two.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv) {
 	int choice = 0;
 	signal(SIGINT, sigint_handler);
 	MADHT1505BA1_object slave0;
+	MADHT1505BA1_object slave1;
 	ret = MADHT1505BA1_master_init();
 	if(ret == -1) {
 		printf("MADHT1505BA1_master_init is err\n");
@@ -28,9 +29,17 @@ int main(int argc, char **argv) {
 	slave0.alias = 0;
 	slave0.position = 0;
 	slave0.cpu_core = 3;
+	slave1.alias = 1;
+	slave1.position = 0;
+	slave1.cpu_core = 3;
 	ret = MADHT1505BA1_slaves_init(&slave0);
 	if(ret == -1) {
 		printf("MADHT1505BA1_slaves_init0 is err\n");
+		return -1;
+	}
+	ret = MADHT1505BA1_slaves_init(&slave1);
+	if(ret == -1) {
+		printf("MADHT1505BA1_slaves_init1 is err\n");
 		return -1;
 	}
 	ret = MADHT1505BA1_master_activate();
@@ -43,9 +52,19 @@ int main(int argc, char **argv) {
 		printf("MADHT1505BA1_slaves_activate0 is err\n");
 		return -1;
 	}
+	ret = MADHT1505BA1_slaves_activate(&slave1);
+	if(ret == -1) {
+		printf("MADHT1505BA1_slaves_activate1 is err\n");
+		return -1;
+	}	
 	ret = MADHT1505BA1_slave_start(&slave0);
 	if(ret == -1) {
 		printf("MADHT1505BA1_slave_start0 is err\n");
+		return -1;
+	}
+	ret = MADHT1505BA1_slave_start(&slave1);
+	if(ret == -1) {
+		printf("MADHT1505BA1_slave_start1 is err\n");
 		return -1;
 	}
 
@@ -60,11 +79,13 @@ int main(int argc, char **argv) {
     	switch(choice) {
     	    case 1:
     	        MADHT1505BA1_motor_start(&slave0);
+    	        MADHT1505BA1_motor_start(&slave1);
     	        choice = 0;
     	        break;
     	        
     	    case 2:
     	        MADHT1505BA1_motor_stop(&slave0);
+    	        MADHT1505BA1_motor_stop(&slave1);
     	        choice = 0;
     	        break;
     	       	        
